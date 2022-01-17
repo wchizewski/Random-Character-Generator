a92a6a9729bfda84f1258f05e3f162b3222a9890
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-01-17 14:48:59 -0700

    monday

---
+++ script.js
@@ -0,0 +1,7 @@
+// Random Character Generator by Will
+// Setup Canvas and Graphics Context
+let cnv = document.getElementById("myCanvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 1000;
+cnv.height = 900;
+
